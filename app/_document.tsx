ec0625e73cb837b3d8ebd694ebca15c2b1d44b43
LEEHYUNGSU <gudtn0506@gmail.com>
2024-12-28 17:34:49 +0900

    seven

---
+++ app/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* 브라우저 주소창 색상 설정 */}
+          <meta name="theme-color" content="#ff5733" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
